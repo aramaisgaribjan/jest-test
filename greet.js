c42990468155ec939030f55f9ed3e8defbf8eef2
Aramais Garibjan <aramo.g@hotmail.de>
2022-02-21 16:07:01 +0100

    create jest tests

--- greet.js
+++ greet.js
@@ -1,4 +1,15 @@
 export function greet(name) {
-  // ????
-  return `Hello, ${name}!`;
+  if (name === "Lara" || name === "CJ" || name === "Jan") {
+    return `Hello, Coach!`;
+  } else if (name === undefined) {
+    return "Hello, stranger!";
+  } else {
+    return `Hello, ${name}!`;
+  }
+}
+
+export function slugify(text) {
+  let string = text.toLowerCase();
+  string.replaceAll(" ", "-");
+  return string;
 }
--- greet.spec.js
+++ greet.spec.js
@@ -1,9 +1,38 @@
 import { greet } from "./greet.js";
+import { slugify } from "./greet.js";
 
 describe("greet", () => {
-  it("returns …", () => {
-    // expect(greet("…")).toBe(…)
+  it("returns 'Hello, Coach!'", () => {
+    expect(greet("Lara")).toBe("Hello, Coach!");
   });
-
-  // add more test here!
+  it("returns 'Hello, Coach!'", () => {
+    expect(greet("CJ")).toBe("Hello, Coach!");
+  });
+  it("returns 'Hello, Coach!'", () => {
+    expect(greet("Jan")).toBe("Hello, Coach!");
+  });
+  it("returns 'Hello, Aramo!'", () => {
+    expect(greet("Aramo")).toBe("Hello, Aramo!");
+  });
+  it("returns 'Hello, stranger!'", () => {
+    expect(greet()).toBe("Hello, stranger!");
+  });
 });
+
+describe("slugify", () => {
+  it("returns 'hello world'", () => {
+    expect(slugify("HELLO WorlD")).toBe("hello world");
+  });
+  it("returns 'hello-world'", () => {
+    expect(slugify("HELLO WorlD")).toBe("hello-world");
+  });
+});
+
+// If the name argument equals one of your coaches, return "Hello Coach!".
+// If there is a name argument, return "Hello <name>!".
+// If there is no argument given, return "Hello stranger!"
+
+// Add a tests and implementation for a new function slugify, that lowercases strings, replaces spaces with "-" and removes umlauts:
+
+// 'I love JavaScript' becomes 'i-love-javascript'
+// 'Ich übe barfuß das Programmieren' becomes 'ich-uebe-barfuss-das-programmieren'
